52ee3ef5fe923a81dd36de0488e0a375cc2c1314
Wuanitawanjiku <wanjikuwuanita@gmail.com>
2021-06-19 10:57:00 +0300

    third commit

--- app/src/main/java/com/example/registration/CoursesActivity.kt
+++ app/src/main/java/com/example/registration/CoursesActivity.kt
@@ -2,10 +2,31 @@ package com.example.registration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CoursesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courses)
+
+        displayContent()
+    }
+
+    fun displayContent(){
+        var rvCourses = findViewById<RecyclerView>(R.id.rvCourses)
+        var courseList = listOf(
+            Course("MB101", "Mobile Development", "Introduction to Android Development with Kotlin", "John Owuor"),
+            Course("BE2101", "Backend Development", "Introduction to Backend Development with Python", "James Mwai"),
+            Course("IOT104", "IoT", "IoT implementation", "Barre Yassin"),
+            Course("FE105", "Frontend Development", "Introduction to Frontend Development with JavaScript", "Purity Maina"),
+            Course("IDX502", "Industrial Design", "Design for everyday life", "Erick Asiago"),
+            Course("UXR301", "UX Research", "UX and UI field research", "Joy Wambui")
+        )
+        var coursesAdapter = CoursesAdapter(courseList)
+        rvCourses.layoutManager = LinearLayoutManager(baseContext)
+        rvCourses.adapter = coursesAdapter
     }
 }
+
+
